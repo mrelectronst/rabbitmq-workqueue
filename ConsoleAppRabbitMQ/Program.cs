9dc4f3e617e46d45fe1d66a8560d668863557bd8
mrelectronst <fhkaraman@outlook.com>
2022-04-13 14:03:35 +0300

    Add project files.

---
+++ ConsoleAppRabbitMQ/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rktcczxq:eBER7Ivsuu2oNFAD-RPxpEFEPZMu99W5@whale.rmq.cloudamqp.com/rktcczxq"); //write AMQP URL
+
+string messageRead;
+
+do
+{
+    Console.Write("Please write your message for 50 times: ");
+    messageRead = Console.ReadLine();
+    if (messageRead != string.Empty && messageRead != null)
+    {
+        PublishMessage(messageRead);
+    }
+    else
+    {
+        Environment.Exit(0);
+    }
+
+} while (messageRead != null);
+
+void PublishMessage(string message)
+{
+    try
+    {
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("workqueue-one", true, false, false);
+
+            Enumerable.Range(0, 40).ToList().ForEach(x =>
+            {
+                var messageBody = Encoding.UTF8.GetBytes(message+$"_{x}");
+
+                channel.BasicPublish(string.Empty, "workqueue-one", null, messageBody);
+
+                Console.WriteLine($"'{message}-{x}' is sended");
+            });
+
+            Console.WriteLine("All messages are sended");
+        }
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+
+}
+
+
---
+++ SubscriberRabbitMQ/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rktcczxq:eBER7Ivsuu2oNFAD-RPxpEFEPZMu99W5@whale.rmq.cloudamqp.com/rktcczxq"); //write AMQP URL
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    channel.QueueDeclare("workqueue-one", true, false, false);
+
+    channel.BasicQos(0,20,false);
+
+    var subscriber = new EventingBasicConsumer(channel);
+
+    channel.BasicConsume("workqueue-one", false, subscriber);
+
+    subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine($"Received Message : {message}");
+
+        channel.BasicAck(e.DeliveryTag, false);
+    };
+}
